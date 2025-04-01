4bebbe43f27b6a95e6d61db55cbb9d237bf060b5
Curtis J Pierce <Curtisjpierce39@gmail.com>
2025-04-01 12:45:51 -0700

    April Fools commit #2

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }], // or your preferred env preset
-        '@babel/preset-react', // Add this line
-    ],
-    transform: {
-        '^.+\\.js$': 'babel-jest', // or '^.+\\.jsx$' if you have .jsx files
-    },
-};
